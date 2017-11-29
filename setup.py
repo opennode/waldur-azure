2767934bba17257db8f75f668b867ba5e1f4a38a
Jenkins <jenkins@opennodecloud.com>
2017-11-29 15:43:29 +0000

    Preparing new release: 0.3.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 setup(
     name='waldur-azure',
-    version='0.3.2',
+    version='0.3.3',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://waldur.com',
